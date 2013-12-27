f390b87735760eb421303dee586c93a2d8d9ec3f
ptkweller <p.weller@cept.at>
2013-12-27 13:30:34 +0100

    Update apache2 package installation.

--- helloworld/recipes/default.rb
+++ helloworld/recipes/default.rb
@@ -5,12 +5,12 @@
 #
 # Install apache
 package 'apache2' do
-  case node[:platform]
-  when 'centos','redhat','fedora','amazon'
-    package_name 'httpd'
-  when 'debian','ubuntu'
-    package_name 'apache2'
-  end
+#  case node[:platform]
+#  when 'centos','redhat','fedora','amazon'
+#    package_name 'httpd'
+#  when 'debian','ubuntu'
+#    package_name 'apache2'
+#  end
   action :install
 end
 
