65ce7626ccd791b239ac6ffbde77063e24a40528
ptkweller <p.weller@cept.at>
2013-12-27 13:14:27 +0100

    Update version.

--- helloworld/metadata.rb
+++ helloworld/metadata.rb
@@ -4,13 +4,8 @@ maintainer_email 'test@test.com'
 license          'Apache 2.0'
 description      'Installs and configures HelloWorld.html'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.1'
+version          '0.1'
 
 recipe 'default', 'Installs HelloWorld.html file'
 
-depends 'apt'
-depends 'yum'
-
 depends 'apache2'
-depends 'nginx'
-depends 'iptables'
