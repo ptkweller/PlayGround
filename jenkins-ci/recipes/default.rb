df32f4d2eec89e031ba022bcc6f9425a2f3044e7
ptkweller <p.weller@cept.at>
2014-01-02 10:40:04 +0100

    yum nginx install

--- jenkins-ci/recipes/default.rb
+++ jenkins-ci/recipes/default.rb
@@ -9,30 +9,34 @@
 #  mode 00755
 #end
 
-#bash "install_something" do
-#  user "ec2-user"
-#  cwd "/tmp"
-#  code <<-EOH
-#    sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
-#    sudo rpm --import http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key
+bash "add_jenkins_repo" do
+  user "ec2-user"
+  cwd "/tmp"
+  code <<-EOH
+    sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
+    sudo rpm --import http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key
 #    sudo yum install jenkins
-#  EOH
-#end
-
-git "/opt/mysources/yum" do
-  repository "https://github.com/opscode-cookbooks/yum.git"
-  reference "master"
-  action :sync
+  EOH
 end
 
-yum_repository 'jenkins-ci' do
-  description "Jenkins CI Repo"
-  baseurl "http://pkg.jenkins-ci.org/redhat/"
-  gpgkey 'http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key'
-  action :create
-end  
+#git "/opt/mysources/yum" do
+#  repository "https://github.com/opscode-cookbooks/yum.git"
+#  reference "master"
+#  action :sync
+#end
 
-yum_package "jenkins-ci" do
+#yum_repository 'jenkins-ci' do
+#  description "Jenkins CI Repo"
+#  baseurl "http://pkg.jenkins-ci.org/redhat/"
+#  gpgkey 'http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key'
+#  action :create
+#end  
+
+yum_package "nginx" do
+  action :install
+end
+
+yum_package "jenkins" do
   action :install
 end
   
